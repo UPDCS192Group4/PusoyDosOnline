d0c0d84c5d58ecbf2aec8b44c127602b77de4970
clydejallorina <clydejallorina@gmail.com>
2022-03-10 13:58:01 +0800

    save current progress to remote (not yet complete)

--- server/PusoyDosOnline/PusoyDosServer/models.py
+++ server/PusoyDosOnline/PusoyDosServer/models.py
@@ -1,3 +1,31 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Lobby(models.Model):
+    """
+    Lobby information
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    websocket = models.CharField(max_length=128, default="", blank=True)
+    owner = models.UUIDField()
+    creation_date = models.DateTimeField()
+    last_activity = models.DateTimeField()
+    status = models.IntegerField() # status is if it's waiting or about to start a game
+
+class User(AbstractUser):
+    """
+    User information
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    rating = models.IntegerField("Elo Rating")
+    current_lobby = models.ForeignKey(Lobby)
+    country_code = models.CharField(max_length=3, defualt="")
+    highest_rating = models.IntegerField()
+    
+    
+    def __str__(self):
+        return self.username
+    
--- server/PusoyDosOnline/PusoyDosServer/views.py
+++ server/PusoyDosOnline/PusoyDosServer/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import (Http404, HttpResponseRedirect, JsonResponse, HttpRequest, HttpResponseBadRequest)
+from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def api_login(request):
+    if request.method != "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username, password)
+        if user is not None:
+            login(request, user)
+            return redirect("/api/profile")
+        else:
+            pass
+    else:
+        pass
+
+def api_register(request):
+    pass
+
+@login_required(login_url="/api/login")
+def api_profile(request):
+    pass
+
+def api_leaderboard(request):
+    pass
