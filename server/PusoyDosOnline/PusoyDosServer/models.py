5c0e4c58cb801b587dbe2f11a34e0660bc250637
clydejallorina <clydejallorina@gmail.com>
2022-03-11 16:04:20 +0800

    working user model

--- server/PusoyDosOnline/PusoyDosServer/models.py
+++ server/PusoyDosOnline/PusoyDosServer/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -10,10 +11,10 @@ class Lobby(models.Model):
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     websocket = models.CharField(max_length=128, default="", blank=True)
-    owner = models.UUIDField()
-    creation_date = models.DateTimeField()
-    last_activity = models.DateTimeField()
-    status = models.IntegerField() # status is if it's waiting or about to start a game
+    owner = models.UUIDField(null=True)
+    creation_date = models.DateTimeField(default=timezone.now)
+    last_activity = models.DateTimeField(default=timezone.now)
+    status = models.IntegerField(default=True) # status is if it's waiting or about to start a game
 
 class User(AbstractUser):
     """
@@ -23,10 +24,10 @@ class User(AbstractUser):
     last_name = None # We don't need these so we'll just set these to NULL
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    rating = models.IntegerField("Elo Rating")
+    rating = models.IntegerField("Elo Rating", default=1000)
     current_lobby = models.ForeignKey(Lobby, on_delete=models.SET_NULL, null=True)
     country_code = models.CharField(max_length=3, default="")
-    highest_rating = models.IntegerField("Max Rating")
+    highest_rating = models.IntegerField("Max Rating", default=1000)
     
     def __str__(self):
         return self.username
