3a795a65d2e3999ba02b29cd1eaa6931e5329ee5
Rosenquartz <tayangelera@gmail.com>
2022-03-17 18:55:20 +0800

    Leaderboards scene

---
+++ project/Leaderboards.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Button_pressed():	
+	yield(get_tree().create_timer(0.5), "timeout")
+	get_tree().change_scene("res://Home.tscn")
