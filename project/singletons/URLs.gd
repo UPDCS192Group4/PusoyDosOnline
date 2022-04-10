eb29164ac434ebf184aa49291a4ae6ca7fbc8663
Lexi <alexis031098@gmail.com>
2022-04-10 22:20:41 +0800

    Add pre-loads for account info and url paths

---
+++ project/singletons/URLs.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var url = "http://localhost:8000" # modify upon deployment
+var register = url + "/api/register/"
+var login = url + "/api/token/"
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
