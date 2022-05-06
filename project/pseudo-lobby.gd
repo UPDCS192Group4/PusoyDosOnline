fb8d9cde5348d98974528f62bd32623fc9dda8f3
Rosenquartz <tayangelera@gmail.com>
2022-05-07 07:41:46 +0800

    Tests

--- project/pseudo-lobby.gd
+++ project/pseudo-lobby.gd
@@ -7,6 +7,8 @@ func _ready():
 	ws.connect("connection_closed", self, "_connection_failed")
 	ws.connect("connection_error", self, "_connection_failed")
 	ws.connect("connection_established", self, "_connection_success")
+	print("PRINTING DIR...")
+	dir(ws)
 
 func _process(_delta):
 	ws.poll()
@@ -16,6 +18,7 @@ func _connection_success(_proto = ""):
 	print("CONNECTED: ", ws.get_connected_host(), ":", ws.get_connected_port())
 	$Label.text = "Connection success!"
 	$Label.visible = true
+	dir(ws)
 	get_tree().change_scene("res://Game.tscn")
 
 func _connection_failed(_was_clean = false):
@@ -28,6 +31,10 @@ func _on_Button_pressed():
 	$ConnectingLabel.visible = true
 	var url = $LineEdit.text
 	var err = ws.connect_to_url(url)
+	
+	print("PRINTING DIR...")
+	dir(ws)
+	
 	ws.poll()
 	if err != OK:
 		print("ERR: ", err)
@@ -35,3 +42,25 @@ func _on_Button_pressed():
 
 func _on_Timer_timeout():
 	$Label.visible = false
+
+func dir(class_instance):
+	var output = {}
+	var methods = []
+	for method in class_instance.get_method_list():
+		methods.append(method.name)
+	
+	output["METHODS"] = methods
+	
+	var properties = []
+	for prop in class_instance.get_property_list():
+		properties.append([prop.name, class_instance.get(prop.name)])
+	output["PROPERTIES"] = properties
+	
+	print("METHODS:")
+	for i in output["METHODS"]:
+		print(i)
+	print("PROPERTIES:")
+	for i in output["PROPERTIES"]:
+		print(i)
+	print(get_stack())
+	return output
