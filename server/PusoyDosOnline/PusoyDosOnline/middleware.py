398e7745b028e4cbc48888b1bd2426f11b8742dd
Rosenquartz <tayangelera@gmail.com>
2022-05-07 09:50:41 +0800

    Revert "i quit"
    This reverts commit ce62aaf944a7c87c9a008d6c72162bc51904838f.

--- server/PusoyDosOnline/PusoyDosOnline/middleware.py
+++ server/PusoyDosOnline/PusoyDosOnline/middleware.py
@@ -12,26 +12,27 @@ class UserAuthMiddleware:
         for name, value in headers:
             if name == b'authorization':
                 return value
+            elif name == b'sec-websocket-protocol':
+                return value
         return None
         
     async def __call__(self, scope, receive, send):
         token = None
         headers = scope["headers"]
-        print("receive: ", receive)
-        print("HEADERS: ", headers)
+        print(headers)
         header = self.filter_headers(headers)
-        #if header:
-        #    raw_token = self.JWTAuth.get_raw_token(header)
+        if header:
+            raw_token = self.JWTAuth.get_raw_token(header)
         
-        #if raw_token:
-        #    try:
-        #        token = self.JWTAuth.get_validated_token(raw_token)
-        #    except: # token did not get validated, either token is no longer valid or was never valid
-        #        token = None
+        if raw_token:
+            try:
+                token = self.JWTAuth.get_validated_token(raw_token)
+            except: # token did not get validated, either token is no longer valid or was never valid
+                token = None
                 
-        #if token:
-        #    scope["user"] = await sync_to_async(self.JWTAuth.get_user)(token)
-        #else:
-        #    scope["user"] = None
+        if token:
+            scope["user"] = await sync_to_async(self.JWTAuth.get_user)(token)
+        else:
+            scope["user"] = None
             
         return await self.app(scope, receive, send)
