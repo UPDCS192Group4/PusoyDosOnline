c5ec4eb3c5d8655bdbdea24f374d9b5140e2b73d
Rosenquartz <tayangelera@gmail.com>
2022-05-08 21:36:37 +0800

    Kick player out old lobbies

--- project/Home/Home.gd
+++ project/Home/Home.gd
@@ -1,16 +1,78 @@
 extends Control
 
 func _ready():
-	pass
+	$ClearRequest.connect("request_completed", self, "_on_ClearRequest_request_completed")
+	$LeaveRequest.connect("request_completed", self, "_on_LeaveRequest_request_completed")
+	$PingRequest.connect("request_completed", self, "_on_PingRequest_request_completed")
+	$ErrorMessage/Label.hide()
 
 func _on_PlayButton_pressed():
 	yield(get_tree().create_timer(0.5), "timeout")
-	openLobbyHandler()
-	pass # Replace with function body.
+	clearLobby()
 		
 func _on_LeaderboardsButton_pressed():
 	yield(get_tree().create_timer(0.5), "timeout")
 	showLeaderboards()
+	
+func clearLobby():
+	var url = URLs.profile
+	var headers = ['Content-Type: application/json', 'Authorization: Bearer ' + URLs.access]
+	var err = $ClearRequest.request(url, headers, false, HTTPClient.METHOD_GET)
+	
+func _on_ClearRequest_request_completed(result, response_code, headers, body):
+	if (response_code != 200 and response_code != 201): 
+		$ErrorMessage/Label.text = "Uh oh!..."
+		$ErrorMessage/Label.show()
+		yield(get_tree().create_timer(2), "timeout")
+		$ErrorMessage/Label.text = "Please contact Big T.."
+		yield(get_tree().create_timer(2), "timeout")
+		get_tree().change_scene("res://Home/Home.tscn")	
+		return
+	
+	var json = JSON.parse(body.get_string_from_utf8())
+	if json.result["lobby"] == null:
+		openLobbyHandler()
+		return
+	LobbyDetails.shorthand = json.result["lobby"]
+	_query_lobby_id()
+	
+func _query_lobby_id():
+	var url = URLs.lobby_init + LobbyDetails.shorthand + "/"
+	var headers = ['Content-Type: application/json', 'Authorization: Bearer ' + URLs.access]
+	print("Pinging ", url)
+	var err = $PingRequest.request(url, headers, false, HTTPClient.METHOD_GET)
+
+func _on_PingRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	if (response_code != 200 and response_code != 201): 
+		$ErrorMessage/Label.text = "Oh no..."
+		$ErrorMessage/Label.show()
+		yield(get_tree().create_timer(2), "timeout")
+		$ErrorMessage/Label.text = "Contact BIG T..."
+		yield(get_tree().create_timer(2), "timeout")
+		get_tree().change_scene("res://Home/Home.tscn")	
+		return
+	print("Ping request results")
+	for i in json.result:
+		print(i, " : ", json.result[i])
+	LobbyDetails.id = json.result["id"]
+	_go_home()
+		
+func _go_home():
+	var url = URLs.lobby_init + LobbyDetails.id + "/leave/"
+	var new_headers = ['Content-Type: application/json', 'Authorization: Bearer ' + URLs.access]
+	var err = $LeaveRequest.request(url, new_headers, false, HTTPClient.METHOD_GET)
+	
+func _on_LeaveRequest_request_completed(result, response_code, headers, body):
+	if (response_code != 200 and response_code != 201): 
+		$ErrorMessage/Label.text = "Leave failed, LOL!..."
+		$ErrorMessage/Label.show()
+		yield(get_tree().create_timer(2), "timeout")
+		$ErrorMessage/Label.text = "Redirecting..."
+		yield(get_tree().create_timer(1), "timeout")
+		get_tree().change_scene("res://Home/Home.tscn")	
+		return
+	openLobbyHandler()
 	
 func openLobbyHandler():
 	get_tree().change_scene("res://Lobby/Lobby.tscn")
--- project/singletons/URLs.gd
+++ project/singletons/URLs.gd
@@ -1,10 +1,11 @@
 extends Node
 
-#var url = "http://localhost:8000" # modify upon deployment
+var url = "http://localhost:8000" # modify upon deployment
 #var url = "http://642f-2001-4451-6b8-9500-38fd-82a4-400a-338b.ngrok.io"
-var url = "http://1cc0-2001-4451-6b8-9500-38fd-82a4-400a-338b.ngrok.io"
+#var url = "http://1cc0-2001-4451-6b8-9500-38fd-82a4-400a-338b.ngrok.io"
 var register = url + "/api/register/"
 var login = url + "/api/token/"
+var profile = url + "/api/users/profile/"
 var leaderboards = url + "/api/users/leaderboard"
 var lobby_init = url + "/api/lobby/casual/"
 
