628d404330e6fa838ff2a1140480b8a44dc555fd
Lexi <alexis031098@gmail.com>
2022-05-10 07:49:02 +0800

    Add profile info variables and update method

--- project/singletons/Account.gd
+++ project/singletons/Account.gd
@@ -1,6 +1,16 @@
 extends Node
 
 var logged_in = false
+var id = -1
+var username = ""
+var rating = -1
+var country_code = "XX"
+var played_games = -1
+var won_games = -1
+var lost_games = -1
+var winstreak = -1
+var friends = []
+
 var _token = ""
 
 func _ready():
@@ -14,4 +24,17 @@ func setToken(token):
 
 func logout():
 	logged_in = false
+	
+func updateInfo(profile_json):
+	# should be the json result already
+	id = profile_json.id
+	username = profile_json.username
+	rating = profile_json.rating
+	country_code = profile_json.country_code
+	played_games = profile_json.played_games
+	won_games = profile_json.won_games
+	lost_games = profile_json.lost_games
+	winstreak = profile_json.winstreak
+	friends = profile_json.friends
+	
 	# _token = "" idk if I should touch this or nah
