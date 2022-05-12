195c2329c9c361082c27ddc50180ce7adf4827d7
Rosenquartz <tayangelera@gmail.com>
2022-05-12 14:54:13 +0800

    Before d storm!

--- project/singletons/URLs.gd
+++ project/singletons/URLs.gd
@@ -1,9 +1,9 @@
 extends Node
 
-#var url = "http://localhost:8000" # modify upon deployment
+var url = "http://localhost:8000" # modify upon deployment
 #var url = "http://642f-2001-4451-6b8-9500-38fd-82a4-400a-338b.ngrok.io"
 #var url = "http://1cc0-2001-4451-6b8-9500-38fd-82a4-400a-338b.ngrok.io"
-var url = "http://07a7-136-158-0-18.ngrok.io"
+#var url = "http://07a7-136-158-0-18.ngrok.io"
 var register = url + "/api/register/"
 var login = url + "/api/token/"
 var profile = url + "/api/users/profile/"
