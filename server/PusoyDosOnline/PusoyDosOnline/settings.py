477cac1ed571b7bc9a5a8035aed15000f342dd46
clydejallorina <clydejallorina@gmail.com>
2022-05-18 15:07:34 +0800

    add ip of deployment server to allowed hosts

--- server/PusoyDosOnline/PusoyDosOnline/settings.py
+++ server/PusoyDosOnline/PusoyDosOnline/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRETKEY", default='django-insecure-5r9a(zi(1)*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", default="False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', '159.65.129.248']
 
 
 # Application definition
